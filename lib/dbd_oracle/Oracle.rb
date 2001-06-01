4fe0ea09ffe2cfa1b92ba4585dc1ff3ed6394feb
Michael Neumann <mneumann@ntecs.de>
2001-06-01 11:04:51 +0000

    improved speed, fixed some bugs

--- lib/dbd_oracle/Oracle.rb
+++ lib/dbd_oracle/Oracle.rb
@@ -1,13 +1,41 @@
 #
-# $Id: Oracle.rb,v 1.1 2001/05/30 12:40:58 michael Exp $
+# $Id: Oracle.rb,v 1.2 2001/06/01 11:04:51 michael Exp $
 # Copyright (c) 2001 by Michael Neumann
+#
+# copied some code from lib/oracle.rb of Oracle 7 Module
 # 
 
 require "oracle"
+class OCIError
+  def to_i
+    if self.to_s =~ /^ORA-(\d+):/
+      return $1.to_i
+    end
+    0
+  end
+end
 
 module DBI
 module DBD
 module Oracle
+
+
+  VARCHAR2 = 1
+  NUMBER = 2
+  INTEGER = 3 ## external
+  FLOAT = 4   ## external
+  LONG = 8
+  ROWID = 11
+  DATE = 12
+  RAW = 23
+  LONG_RAW = 24
+  UNSIGNED_INT = 68 ## external
+  CHAR = 96
+  MLSLABEL = 105
+
+
+
+
 
 VERSION          = "0.1"
 USED_DBD_VERSION = "0.1"
@@ -31,15 +59,20 @@ class Driver < DBI::BaseDriver
     # connect to database
     handle = ::ORAconn.logon(user, auth, dbname)
     return Database.new(handle, attr)
+  rescue OCIError => err
+    raise DBI::DatabaseError.new(err.to_i, err.message)
   end
 
-end
+end # class Driver
+
 
 class Database < DBI::BaseDatabase
  
   def disconnect
     @handle.rollback
     @handle.logoff 
+  rescue OCIError => err
+    raise DBI::DatabaseError.new(err.to_i, err.message)
   end
 
   def ping
@@ -61,9 +94,7 @@ class Database < DBI::BaseDatabase
   end
 
   def prepare(statement)
-    cursor = @handle.open
-    cursor.parse(statement)
-    Statement.new(cursor)
+    Statement.new(@handle, statement)
   end
 
   def []=(attr, value)
@@ -78,14 +109,21 @@ class Database < DBI::BaseDatabase
       raise NotSupportedError
     end
     @attr[attr] = value
+
+  rescue OCIError => err
+    raise DBI::DatabaseError.new(err.to_i, err.message)
   end
 
   def commit
     @handle.commit
+  rescue OCIError => err
+    raise DBI::DatabaseError.new(err.to_i, err.message)
   end
 
   def rollback
     @handle.rollback
+  rescue OCIError => err
+    raise DBI::DatabaseError.new(err.to_i, err.message)
   end
 
 end # class Database
@@ -93,10 +131,9 @@ end # class Database
 
 class Statement < DBI::BaseStatement
 
-  def initialize(handle)
-    @handle = handle
-    @params = []
-    @arr = []
+  def initialize(handle, statement)
+    parse(handle, statement)
+    @arr = Array.new(@ncols)
   end
 
   def bind_param(param, value, attribs)
@@ -105,46 +142,38 @@ class Statement < DBI::BaseStatement
     ##
     # which SQL type?
     #
-    if value.kind_of? Integer
-      vtype = ::Oracle::INTEGER
-    elsif value.is_a? Float
-      vtype = ::Oracle::FLOAT
-    elsif value.is_a? DBI::Binary
-      vtype = ::Oracle::LONG_RAW
+    #if value.kind_of? Integer
+    #  vtype = INTEGER
+    #elsif value.is_a? Float
+    #  vtype = FLOAT
+
+    if value.is_a? DBI::Binary
+      vtype = LONG_RAW
     else
-      vtype = ::Oracle::VARCHAR2
+      vtype = VARCHAR2
     end
 
     param = ":#{param}" if param.is_a? Fixnum
     @handle.bindrv(param, value.to_s, vtype)
+   
+  rescue OCIError => err
+    raise DBI::DatabaseError.new(err.to_i, err.message)
+  end
+
+  def cancel
+    @handle.cancel
   end
 
   def execute
-    colnr = 1
-    loop {
-      colinfo = @handle.describe(colnr)
-
-      break if colinfo.nil?
-
-      collength, coltype = colinfo[3], colinfo[1]
-
-      coltype = case coltype
-        when ::Oracle::NUMBER then ::Oracle::FLOAT
-        when ::Oracle::DATE   then ::Oracle::VARCHAR2
-        else coltype
-      end
-
-      @handle.define(colnr, collength, coltype)
-
-      colnr += 1
-    }
-    @ncols = colnr - 1
-
     @rows = @handle.exec
+  rescue OCIError => err
+    raise DBI::DatabaseError.new(err.to_i, err.message)
   end
 
   def finish
     @handle.close
+  rescue OCIError => err
+    raise DBI::DatabaseError.new(err.to_i, err.message)
   end
 
   def fetch
@@ -156,24 +185,69 @@ class Statement < DBI::BaseStatement
     end 
 
     @arr
+  rescue OCIError => err
+    raise DBI::DatabaseError.new(err.to_i, err.message)
   end
 
   def column_info
-    retval = []
-    colnr = 1
-    loop {
-      colinfo = @handle.describe(colnr)
-      break if colinfo.nil?
-      
-      retval << {'name' => colinfo[2] }
-
-      colnr += 1
-    }
-    retval
+    @colinfo
   end
 
   def rows
     @rows
+  end
+
+  private # ---------------------------------------------------
+
+  def parse(handle, statement)
+    @handle = handle.open
+
+    begin
+      @handle.parse(statement)
+    rescue OCIError => err
+      retry if err.to_i == 3123  ## block
+    end   
+
+    colnr = 1
+    @colinfo = []
+    loop {
+      colinfo = @handle.describe(colnr)
+      break if colinfo.nil?
+
+      @colinfo << {'name' => colinfo[2]}
+
+      collength, coltype = colinfo[3], colinfo[1]
+
+      collength, coltype = case coltype
+##      when NUMBER
+##        [0, FLOAT]
+      when NUMBER
+        [40, VARCHAR2]
+      when VARCHAR2, CHAR
+        [(collength*1.5).ceil, VARCHAR2]
+      when LONG
+        [65535, LONG]
+      when LONG_RAW
+        [65535, LONG_RAW]
+      else
+        [collength, VARCHAR2]
+      end
+ 
+
+      #coltype = case coltype
+      #  when ::Oracle::NUMBER then ::Oracle::FLOAT
+      #  when ::Oracle::DATE   then ::Oracle::VARCHAR2
+      #  else coltype
+      #end
+
+      @handle.define(colnr, collength, coltype)
+
+      colnr += 1
+    }
+    @ncols = colnr - 1
+
+  rescue OCIError => err
+    raise DBI::DatabaseError.new(err.to_i, err.message)
   end
 
 end
