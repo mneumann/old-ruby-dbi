fead62fbb18fdbb6e24307fc7dfb52a88f6d3fda
Rainer Perl <rainer.perl@sprytech.com>
2001-06-02 19:20:08 +0000

    applied patch from Akinori MUSHA

--- ext/dbd_sybase/extconf.rb
+++ ext/dbd_sybase/extconf.rb
@@ -1,11 +1,6 @@
 require 'mkmf'
 
-# You may need to change these parameters
-FREETDSDIR = "/usr/local/freetds"
-# end of parameters
-
-$CFLAGS = "-I#{FREETDSDIR}/include"
-$LDFLAGS = "-L#{FREETDSDIR}/lib"
+dir_config("freetds", "/usr/local/freetds")
 $libs = "-ltds"
 
 create_makefile("dbd_sybase")
