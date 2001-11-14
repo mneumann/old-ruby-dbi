23891322e0c544dbf42556d4af7c9d2fe51c7553
Michael Neumann <mneumann@ntecs.de>
2001-11-14 14:11:08 +0000

    improved blob test

--- lib/dbd_pg/test/test_blob.rb
+++ lib/dbd_pg/test/test_blob.rb
@@ -1,4 +1,4 @@
-#$Id: test_blob.rb,v 1.1 2001/11/12 19:40:55 michael Exp $
+#$Id: test_blob.rb,v 1.2 2001/11/14 14:11:08 michael Exp $
 
 require "dbi"
 
@@ -14,9 +14,42 @@ DBI.connect("dbi:Pg:michael", "michael", "michael") do |dbh|
   dbh.do("INSERT INTO blob_test (name, data) VALUES (?,?)",
     "test", DBI::Binary.new(DATA))
 
-  oid = dbh.select_one("SELECT data FROM blob_test")['data']
-  if dbh.func(:blob_read, oid) != DATA
-    raise "Test failed"
+  blob = dbh.func(:blob_create, PGlarge::INV_WRITE)
+  blob.open
+  blob.write DATA
+  
+  dbh.do("INSERT INTO blob_test (name, data) VALUES (?,?)",
+    "test (2)", blob.oid)
+  blob.close
+
+  dbh.select_all("SELECT name, data FROM blob_test") do |name, data|
+    print name, ": "
+
+    # (1)
+    if dbh.func(:blob_read, data) == DATA
+      print "ok, "
+    else
+      print "wrong, "
+    end
+
+    # (2)
+    dbh.func(:blob_export, data, '/tmp/dbitest')
+    if File.readlines('/tmp/dbitest').to_s == DATA
+      print "ok, "
+    else
+      print "wrong, "
+    end
+
+    # (3)
+    blob = dbh.func(:blob_open, data, PGlarge::INV_READ)  
+    blob.open
+    if blob.read == DATA
+      puts "ok"
+    else
+      puts "wrong"
+    end
+    blob.close
+
   end
 
 end
