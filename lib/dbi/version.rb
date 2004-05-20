f2fbb0a3ed362d16054713a071fbc5a2f0d40ca0
Michael Neumann <mneumann@ntecs.de>
2004-05-20 10:36:56 +0000

    increased version

--- lib/dbi/version.rb
+++ lib/dbi/version.rb
@@ -1,9 +1,9 @@
 #
-# $Id: version.rb,v 1.8 2004/04/22 20:10:22 mneumann Exp $
+# $Id: version.rb,v 1.9 2004/05/20 10:36:56 mneumann Exp $
 #
 
 module DBI
 
-VERSION = "0.0.22"
+VERSION = "0.0.23"
 
 end
